5511ba5c33fe0751ab3f1bf74bb17cbb79385aa1
Saminuddin Shaikh <saminuddins2@gmil.com>
2023-08-23 19:21:13 +0530

    ecom launched

--- client/src/components/UserCard.js
+++ client/src/components/UserCard.js
@@ -3,15 +3,14 @@ import Avatar from "./Avatar";
 import { Link } from "react-router-dom";
 
 const UserCard = ({ user, border, handleClose }) => {
-
   const handleCloseAll = () => {
-    handleClose && handleClose()
-  }
-  
+    handleClose && handleClose();
+  };
+
   return (
     <div className={`d-flex p-2 align-items-center ${border}`}>
       <div>
-        <Link  to={`/profile/${user._id}`}  onClick={handleCloseAll} className="d-flex align-items-center">
+        <Link to={`/profile/${user._id}`} onClick={handleCloseAll} className="d-flex align-items-center">
           <Avatar size="big-avatar" src={user.avatar} />
           <div className="ml-1" style={{ transform: "translateY(-2px)" }}>
             <span className="d-block">{user.username}</span>
